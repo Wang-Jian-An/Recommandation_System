6335722230aff4b8425ce139db0151a08210e283
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2021-10-10 00:20:17 +0800

    Generate Pearson and Cosine

--- User-based-CF.py
+++ User-based-CF.py
@@ -4,7 +4,7 @@ from tqdm.contrib import itertools
 
 from scipy import stats
 
-def compute_correlation():
+def compute_correlation(user_item_matrix_data, corr_methods):
     # Impute zero to NaN
     impute_zero_user_item_matrix_data = user_item_matrix_data.fillna(0)
 
@@ -12,10 +12,17 @@ def compute_correlation():
     one_user_list = list()
     two_user_list = list()
     user_user_correlation = list()
-    for one_user, two_user in itertools.product(impute_zero_user_item_matrix_data.index, impute_zero_user_item_matrix_data.index):
-        one_user_list.append(one_user)
-        two_user_list.append(two_user)
-        user_user_correlation.append(stats.pearsonr(impute_zero_user_item_matrix_data.loc[one_user, :], impute_zero_user_item_matrix_data.loc[two_user, :])[0])
+
+    if corr_methods == "pearson":
+        for one_user, two_user in itertools.product(impute_zero_user_item_matrix_data.index, impute_zero_user_item_matrix_data.index):
+            one_user_list.append(one_user)
+            two_user_list.append(two_user)
+            user_user_correlation.append(stats.pearsonr(impute_zero_user_item_matrix_data.loc[one_user, :], impute_zero_user_item_matrix_data.loc[two_user, :])[0])
+    elif corr_methods == "cosine":
+        for one_user, two_user in itertools.product(impute_zero_user_item_matrix_data.index, impute_zero_user_item_matrix_data.index):
+            one_user_list.append(one_user)
+            two_user_list.append(two_user)
+            user_user_correlation.append(stats.pearsonr(impute_zero_user_item_matrix_data.loc[one_user, :], impute_zero_user_item_matrix_data.loc[two_user, :])[0])
 
     user_user_correlation_data = pd.crosstab(index=np.array(one_user_list), \
                                             columns=np.array(two_user_list),\
