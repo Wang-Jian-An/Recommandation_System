a7d7b1ec058a25602ea51377ce6c03945367f36d
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2021-10-10 00:18:30 +0800

    Create user-user interaction

---
+++ User-based-CF.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from tqdm.contrib import itertools
+
+from scipy import stats
+
+def compute_correlation():
+    # Impute zero to NaN
+    impute_zero_user_item_matrix_data = user_item_matrix_data.fillna(0)
+
+    # Compute the correlation between two user
+    one_user_list = list()
+    two_user_list = list()
+    user_user_correlation = list()
+    for one_user, two_user in itertools.product(impute_zero_user_item_matrix_data.index, impute_zero_user_item_matrix_data.index):
+        one_user_list.append(one_user)
+        two_user_list.append(two_user)
+        user_user_correlation.append(stats.pearsonr(impute_zero_user_item_matrix_data.loc[one_user, :], impute_zero_user_item_matrix_data.loc[two_user, :])[0])
+
+    user_user_correlation_data = pd.crosstab(index=np.array(one_user_list), \
+                                            columns=np.array(two_user_list),\
+                                            values=user_user_correlation)
+    return user_user_correlation_data
