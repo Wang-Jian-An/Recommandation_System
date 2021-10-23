6396925502db5ca5b5e7e475342f9f8bd284c7e5
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2021-10-23 13:16:49 +0800

    MF完成

--- Matrix_Factorization.py
+++ Matrix_Factorization.py
@@ -7,6 +7,7 @@ warnings.filterwarnings("ignore")
 
 from datetime import datetime
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import *
 from tqdm import tqdm
 import itertools
 
@@ -16,145 +17,73 @@ import torch.nn as nn
 from sklearn.metrics import mean_squared_error
 import math
 
-# Matrix Factorization Model
-class Model(nn.Module):
-    def __init__(self, num_user_id, num_item_id, num_features, all_true_user_item_matrix):
-        super(Model, self).__init__()
-        self.p_matrix = torch.rand(num_user_id, num_features)
-        self.q_matrix = torch.rand(num_features, num_item_id)
+class matrix_factorization():
+    def __init__(self, true_user_item_matrix, num_features):
+        """
+        true_user_item_matrix：user與item的matrix，沒有補過值
+        """
+        self.user_id_list = list(true_user_item_matrix.index)
+        self.item_id_list = list(true_user_item_matrix.columns)
+
+        # 辨識該值是否真的有值
+        self.identify_value_exist = torch.from_numpy( true_user_item_matrix.isna().astype("float").values )
+        self.true_user_item_matrix = self.preprocessing_user_item_matrix(true_user_item_matrix)
+
+        self.p_matrix = torch.randn(size=(len(self.user_id_list), num_features), requires_grad=True)
+        self.q_matrix = torch.randn(size=(num_features, len(self.item_id_list)), requires_grad=True)
 
         # 計算global mean
-        self.global_mean = all_true_user_item_matrix.values.reshape(-1, 1)[torch.where(all_true_user_item_matrix.values.reshape(-1, 1) > 0)[0]].mean()
+        self.global_mean = torch.mean( self.true_user_item_matrix.flatten()[self.true_user_item_matrix.flatten().nonzero()] )
 
         # 計算bias of user and bias of item
-        self.bu = list(map(lambda x: np.mean( all_true_user_item_matrix.loc[x, :].values[np.where(all_true_user_item_matrix.loc[x, :].values > 0)[0]] - self.global_mean), list(all_true_user_item_matrix.index)))
-        self.bi = list(map(lambda x: np.mean( all_true_user_item_matrix.loc[:, x].values[np.where(all_true_user_item_matrix.loc[:, x].values > 0)[0]] - self.global_mean), list(all_true_user_item_matrix.columns)))
+        self.bu = torch.Tensor(list(map(lambda x: torch.mean(x[x.nonzero()]), self.true_user_item_matrix ))).reshape(shape=(-1, 1)) - self.global_mean
+        self.bi = torch.Tensor(list(map(lambda x: torch.mean(x[x.nonzero()]), torch.transpose(self.true_user_item_matrix, 0, 1) ))).reshape(shape=(1, -1)) - self.global_mean
         return
 
-    def forward(self):
-        X = self.q_matrix(self.p_matrix)
-        return X
-
-class Loss_function(nn.Module):
-    def __init__(self):
-        super(Loss_function, self).__init__()
-        return
-
-    def forward(self, true_user_item_matrix, pred_user_item_matrix, p_matrix, q_matrix):
-        return
-
-class Training():
-    def __init__(self):
-        return
-
-    def fit():
-        return
+    def preprocessing_user_item_matrix(self, true_user_item_matrix):
+        # 把NaN全部補零
+        fill_user_item_matrix_data = true_user_item_matrix.fillna(0)
+        return torch.from_numpy(fill_user_item_matrix_data.values)
     
-    def predict():
+    def fit(self, epochs, learning_rate, regularization_rate, bias_or_not):
+        # 建立空的儲存以存取Loss
+        self.train_loss = list()
+
+        # 定義loss function
+        loss_func = nn.MSELoss()
+
+        # 定義optimizer
+        optimizer = torch.optim.SGD([self.p_matrix, self.q_matrix], lr=learning_rate, weight_decay=regularization_rate)
+
+        for epoch in range(epochs):
+            if bias_or_not == False:
+                yhat = torch.tensordot(self.p_matrix, self.q_matrix, dims=([1], [0]))
+            else:
+                yhat = torch.tensordot(self.p_matrix, self.q_matrix, dims=([1], [0]))+self.bu+self.bi+self.global_mean
+                
+            yhat = yhat * self.identify_value_exist
+
+            loss = loss_func(yhat, self.true_user_item_matrix)
+            self.train_loss.append(loss.item())
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            print(f"=== Epoch: {epoch} Train Loss: {loss.item()}")
         return
 
-    def evaluate():
+    def predict(self, user_id, item_id):
+        yhat = torch.tensordot(self.p_matrix, self.q_matrix, dims=([1], [0]))
+        yhat_dataframe = pd.DataFrame(yhat.detach().numpy(), index=self.user_id_list, columns=self.item_id_list)
+        return yhat_dataframe.loc[user_id, item_id]
+
+    def evaluate(self, testdata):
+        """
+        testdata：data.frame，<user_id, item_id, rating, (timestamp)>
+        """
+        testdata["yhat"] = list(map(lambda user, item: self.predict(user, item), tqdm(testdata.iloc[:, 0]), testdata.iloc[:, 1]))
+        print(f"MSE: {mean_squared_error(y_true=testdata.iloc[:, 2], y_pred=testdata['yhat'])}\nr2_score: {r2_score(y_true=testdata.iloc[:, 2], y_pred=testdata['yhat'])}")
         return
 
-
-
-
-
-class Matrix_Factorization_with_bias():
-    def __init__(self, num_user_id, num_item_id, num_features, all_true_user_item_matrix):
-        self.p_matrix = np.random.random(size=(num_user_id, num_features)).astype("float")
-        self.q_matrix = np.random.random(size=(num_features, num_item_id)).astype("float")
-
-        # 計算global mean
-        self.global_mean = all_true_user_item_matrix.values.reshape(-1, 1)[np.where(all_true_user_item_matrix.values.reshape(-1, 1) > 0)[0]].mean()
-
-        # 計算bias of user and bias of item
-        self.bu = list(map(lambda x: np.mean( all_true_user_item_matrix.loc[x, :].values[np.where(all_true_user_item_matrix.loc[x, :].values > 0)[0]] - self.global_mean), list(all_true_user_item_matrix.index)))
-        self.bi = list(map(lambda x: np.mean( all_true_user_item_matrix.loc[:, x].values[np.where(all_true_user_item_matrix.loc[:, x].values > 0)[0]] - self.global_mean), list(all_true_user_item_matrix.columns)))
-
-        self.bu, self.bi = np.array([i if i > 0 else 0 for i in self.bu]).reshape(-1, 1), np.array([i if i > 0 else 0 for i in self.bi]).reshape(1, -1)
+    def save_model(self):
         return
-
-    # 預測Rating
-    def MF_predict_with_bias(self):
-        return np.dot(self.p_matrix, self.q_matrix) + self.bu + self.bi + self.global_mean
-
-    # 2. 建立Loss function→不使用pytorch class
-    def loss_func_with_bias(self, true_user_item_matrix, pred_user_item_matrix, _lambda_=1e-3):
-        """
-        true_user_item_matrix: 有遺失值的data.frame
-        pred_user_item_matrix: 由模型計算出來的ndarray
-        """
-        # 2.1 先辨識出該值是否為有值
-        identify_true_value_matrix = (true_user_item_matrix > 0).astype("int")
-
-        # 2.4 把真實值遺失值的部分補零，並且把它轉成ndarray
-        true_user_item_matrix = true_user_item_matrix.fillna(0).values  
-
-        # 2.5 計算y_true-y_pred
-        self.true_minus_pred = true_user_item_matrix-(pred_user_item_matrix*identify_true_value_matrix.values)
-
-        # 2.4 計算loss
-        loss = np.power(self.true_minus_pred, 2).sum()/2 + \
-            _lambda_ * (np.power(self.p_matrix, 2).sum() + np.power(self.q_matrix, 2).sum() + np.power(self.bu, 2).sum() + np.power(self.bi, 2).sum() + np.power(self.global_mean, 2).sum())/2
-        return loss
-
-    # 3. 計算gradients
-    def compute_gradients_with_bias(self, _lambda_=1e-3):
-        self.gradient_p_matrix = np.dot(self.true_minus_pred, -1 * self.q_matrix.T).sum() + _lambda_ * math.sqrt(np.power(self.p_matrix, 2).sum())
-        self.gradient_q_matrix = np.dot(self.true_minus_pred.T, -1 * self.p_matrix).sum() + _lambda_ * math.sqrt(np.power(self.q_matrix, 2).sum())
-        self.gradient_bu_matrix = (self.true_minus_pred * -1).sum() + _lambda_ * math.sqrt( np.power(self.bu, 2).sum() )
-        self.gradient_bi_matrix = (self.true_minus_pred * -1).sum() + _lambda_ * math.sqrt( np.power(self.bi, 2).sum() )
-        self.gradient_global_mean_matrix = self.true_minus_pred * -1 + _lambda_ * math.sqrt( np.power(self.global_mean, 2).sum() )
-        return 
-
-    # 4. Weight Updated
-    def weight_updated_with_bias(self, learning_rate):
-        self.p_matrix -= learning_rate * self.gradient_p_matrix
-        self.q_matrix -= learning_rate * self.gradient_q_matrix
-        self.bu -= learning_rate * self.gradient_bu_matrix
-        self.bi -= learning_rate * self.gradient_bi_matrix
-        self.global_mean -= learning_rate * self.gradient_global_mean_matrix
-        return 
-
-class Matrix_Factorization_with_nobias():
-    def __init__(self, num_user_id, num_item_id, num_features):
-        self.p_matrix = np.random.random(size=(num_user_id, num_features)).astype("float")
-        self.q_matrix = np.random.random(size=(num_features, num_item_id)).astype("float")
-        return
-
-    # 預測Rating
-    def MF_predict_with_nobias(self):
-        return np.dot(self.p_matrix, self.q_matrix)
-
-    # 2. 建立Loss function→不使用pytorch class
-    def loss_func_with_nobias(self, true_user_item_matrix, pred_user_item_matrix, _lambda_=1e-3):
-        """
-        true_user_item_matrix: 有遺失值的data.frame
-        pred_user_item_matrix: 由模型計算出來的ndarray
-        """
-        # 2.1 先辨識出該值是否為有值
-        identify_true_value_matrix = (true_user_item_matrix > 0).astype("int")
-
-        # 2.4 把真實值遺失值的部分補零，並且把它轉成ndarray
-        true_user_item_matrix = true_user_item_matrix.fillna(0).values  
-
-        # 2.5 計算y_true-y_pred
-        self.true_minus_pred = true_user_item_matrix-(pred_user_item_matrix*identify_true_value_matrix.values)
-
-        # 2.4 計算loss
-        loss = np.power(self.true_minus_pred, 2).sum()/2 + \
-            _lambda_ * (np.power(self.p_matrix, 2).sum() + np.power(self.q_matrix, 2).sum() )/2
-        return loss
-
-    # 3. 計算gradients
-    def compute_gradients_with_nobias(self, _lambda_=1e-3):
-        self.gradient_p_matrix = np.dot(self.true_minus_pred, -1 * self.q_matrix.T).sum() + _lambda_ * math.sqrt(np.power(self.p_matrix, 2).sum())
-        self.gradient_q_matrix = np.dot(self.true_minus_pred.T, -1 * self.p_matrix).sum() + _lambda_ * math.sqrt(np.power(self.q_matrix, 2).sum())
-        return 
-
-    # 4. Weight Updated
-    def weight_updated_with_nobias(self, learning_rate):
-        self.p_matrix -= learning_rate * self.gradient_p_matrix
-        self.q_matrix -= learning_rate * self.gradient_q_matrix
-        return 
