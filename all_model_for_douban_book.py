cf7699a18cce1482825f2712e14d29aeaa65fded
Jian-An Wang <58442343+Wang-Jian-An@users.noreply.github.com>
2021-10-30 20:25:44 +0800

    Douban Book Pipeline

--- all_model_for_douban_book.py
+++ all_model_for_douban_book.py
@@ -101,8 +101,8 @@ class fm_model(nn.Module):
         self.book_publisher_book_year = self.book_publisher * self.book_year
 
         # Concatenate
-        self.all = torch.cat((self.user_age_user_occupation, self.user_age_movie_genre, self.user_occupation_movie_genre,\
-                              self.user_age_weight, self.user_occupation_weight, self.movie_genre_weight), dim=-1) 
+        self.all = torch.cat((self.user_group_book_author, self.user_group_book_publisher, self.user_group_book_year,\
+                              self.book_author_book_publisher, self.book_author_book_year, self.book_publisher_book_year), dim=-1) 
 
         # Decoder
         X = self.decoder(self.all)
@@ -148,8 +148,8 @@ class fnn_model(nn.Module):
         self.book_year_weight = self.book_year_weight_linear(book_year_feature) # shape = (batch_size, 1)
 
         # Concatenate
-        self.all = torch.cat((self.user_age_embedding, self.user_occupation_embedding, self.movie_genre_embedding,\
-                              self.user_age_weight, self.user_occupation_weight, self.movie_genre_weight), dim=-1) 
+        self.all = torch.cat((self.user_group_book_author, self.user_group_book_publisher, self.user_group_book_year,\
+                              self.book_author_book_publisher, self.book_author_book_year, self.book_publisher_book_year), dim=-1) 
 
         # Decoder
         X = self.decoder(self.all)
@@ -218,13 +218,16 @@ class ipnn_model(nn.Module):
         self.book_year_weight = self.book_year_weight_linear(book_year_feature) # shape = (batch_size, 1)
 
         # Inner product
-        self.user_age_user_occupation = self.user_age_embedding * self.user_occupation_embedding # shape=(batch_size, num_features)
-        self.user_age_movie_genre = self.user_age_embedding * self.movie_genre_embedding # shape=(batch_size, num_features)
-        self.user_occupation_movie_genre = self.user_occupation_embedding * self.movie_genre_embedding # shape=(batch_size, num_features)
+        self.user_group_book_author = self.user_group * self.book_author
+        self.user_group_book_publisher = self.user_group * self.book_publisher
+        self.user_group_book_year = self.user_group * self.book_year
+        self.book_author_book_publisher = self.book_author * self.book_publisher
+        self.book_author_book_year = self.book_author * self.book_year
+        self.book_publisher_book_year = self.book_publisher * self.book_year
 
         # Concatenate
-        self.all = torch.cat((self.user_age_user_occupation, self.user_age_movie_genre, self.user_occupation_movie_genre,\
-                              self.user_age_weight, self.user_occupation_weight, self.movie_genre_weight), dim=-1) 
+        self.all = torch.cat((self.user_group_book_author, self.user_group_book_publisher, self.user_group_book_year,\
+                              self.book_author_book_publisher, self.book_author_book_year, self.book_publisher_book_year), dim=-1) 
 
         # Decoder
         X = self.decoder(self.all)
@@ -304,7 +307,7 @@ class neumf(nn.Module):
 class opnn_model(nn.Module):
     def __init__(self, num_user_group, num_book_author, num_book_publisher, num_book_year, num_features, methods):
         super(opnn_model, self).__init__()
-        num_decoder = 3*(num_features**2)+3
+        num_decoder = 6*(num_features**2)+4
         self.methods = methods
         self.user_group = nn.Linear(num_user_group, num_features)
         self.book_author = nn.Linear(num_book_author, num_features)
@@ -324,7 +327,7 @@ class opnn_model(nn.Module):
         return
 
     def forward(self, user_group_feature, book_author_feature, book_publisher_feature, book_year_feature):
-        model_batch_size = user_age_feature.size()[0]
+        model_batch_size = user_group_feature.size()[0]
 
         # Embedding Learning
         self.user_group_embedding = self.user_group(user_group_feature) # shape=(batch_size, embedding)
@@ -340,21 +343,34 @@ class opnn_model(nn.Module):
         # 1. 增加某一維度 (batch_size, num_features, 1) * (batch_size, 1, num_features)
         # 2. 做Inner product (batch_size, num_features, num_features)
         # 3. Flatten
-        self.user_age_user_occupation =\
-            torch.tensordot(torch.unsqueeze(self.user_age_embedding, 1), self.user_occupation_embedding, dims=([1], [0])) # shape=(batch_size, num_features)
-        self.user_age_user_occupation = torch.reshape(self.user_age_user_occupation, shape=(model_batch_size, -1))
+        self.user_group_book_author =\
+            torch.tensordot(torch.unsqueeze(self.user_group_embedding, 1), self.book_author_embedding, dims=([1], [0])) # shape=(batch_size, num_features)
+        self.user_group_book_author = torch.reshape(self.user_group_book_author, shape=(model_batch_size, -1))
        
-        self.user_age_movie_genre =\
-            torch.tensordot(torch.unsqueeze(self.user_age_embedding, 1), self.movie_genre_embedding, dims=([1], [0])) # shape=(batch_size, num_features)
-        self.user_age_movie_genre = torch.reshape(self.user_age_movie_genre, shape=(model_batch_size, -1))
+        self.user_group_book_publisher =\
+            torch.tensordot(torch.unsqueeze(self.user_group_embedding, 1), self.book_publisher_embedding, dims=([1], [0])) # shape=(batch_size, num_features)
+        self.user_group_book_publisher = torch.reshape(self.user_group_book_publisher, shape=(model_batch_size, -1))
         
-        self.user_occupation_movie_genre =\
-            torch.tensordot(torch.unsqueeze(self.user_occupation_embedding, 1), self.movie_genre_embedding, dims=([1], [0])) # shape=(batch_size, num_features)
-        self.user_occupation_movie_genre = torch.reshape(self.user_occupation_movie_genre, shape=(model_batch_size, -1))
+        self.user_group_book_year =\
+            torch.tensordot(torch.unsqueeze(self.user_group_embedding, 1), self.book_year_embedding, dims=([1], [0])) # shape=(batch_size, num_features)
+        self.user_group_book_year = torch.reshape(self.user_group_book_year, shape=(model_batch_size, -1))
+
+        self.book_author_book_publisher =\
+            torch.tensordot(torch.unsqueeze(self.book_author_embedding, 1), self.book_book_publisher, dims=([1], [0]))
+        self.book_author_book_publisher = torch.reshape(self.book_author_book_publisher, shape=(model_batch_size, -1))
+
+        self.book_author_book_year =\
+            torch.tensordot(torch.unsqueeze(self.book_author_embedding, 1), self.book_year_embedding, dims=([1], [0]))
+        self.book_author_book_year = torch.reshape(self.book_author_book_year, shape=(model_batch_size, -1))
+
+        self.book_publisher_book_year =\
+            torch.tensordot(torch.unsqueeze(self.book_publisher_embedding, 1), self.book_year_embedding, dims=([1], [0]))
+        self.book_publisher_book_year = torch.reshape(self.book_publisher_book_year, shape=(model_batch_size, -1))
 
         # Concatenate
-        self.all = torch.cat((self.user_age_user_occupation, self.user_age_movie_genre, self.user_occupation_movie_genre,\
-                              self.user_age_weight, self.user_occupation_weight, self.movie_genre_weight), dim=-1) 
+        self.all = torch.cat((self.user_group_book_author, self.user_group_book_publisher, self.user_group_book_year,\
+                              self.book_author_book_publisher, self.book_author_book_year, self.book_publisher_book_year,\
+                              self.user_group_weight, self.book_author_weight, self.book_publisher_weight, self.book_year_weight), dim=-1) 
 
         # Decoder
         X = self.decoder(self.all)
@@ -400,17 +416,94 @@ class pin_model(nn.Module):
         self.book_year_weight = self.book_year_weight_linear(book_year_feature) # shape = (batch_size, 1)
 
         # Inner product
-        self.user_age_user_occupation = self.user_age_embedding * self.user_occupation_embedding # shape=(batch_size, num_features)
-        self.user_age_movie_genre = self.user_age_embedding * self.movie_genre_embedding # shape=(batch_size, num_features)
-        self.user_occupation_movie_genre = self.user_occupation_embedding * self.movie_genre_embedding # shape=(batch_size, num_features)
+        self.user_group_book_author = self.user_group * self.book_author
+        self.user_group_book_publisher = self.user_group * self.book_publisher
+        self.user_group_book_year = self.user_group * self.book_year
+        self.book_author_book_publisher = self.book_author * self.book_publisher
+        self.book_author_book_year = self.book_author * self.book_year
+        self.book_publisher_book_year = self.book_publisher * self.book_year
 
         # Concatenate   
-        self.all = F.tanh(torch.cat((self.user_age_user_occupation, self.user_age_movie_genre, self.user_occupation_movie_genre), dim=-1) )
+        self.all = F.tanh(torch.cat((self.user_group_book_author,
+                                     self.user_group_book_publisher,
+                                     self.user_group_book_year,
+                                     self.book_author_book_publisher,
+                                     self.book_author_book_year,
+                                     self.book_publisher_book_year), dim=-1) )
 
         # Decoder
         X = self.decoder(self.all)
 
         # identify regression or classification task
+        if self.methods == "regression":
+            return X
+        else:
+            return nn.Sigmoid()(X)
+
+
+# 建構模型→Movielens
+class wide_deep_model(nn.Module):
+    def __init__(self, num_user_group, num_book_author, num_book_publisher, num_book_year, num_features, methods):
+        super(wide_deep_model, self).__init__()
+        num_deep_decoder = 6 * num_features
+        num_decoder = num_user_group + num_book_author + num_book_publisher + num_book_year + int(round(num_deep_decoder/4, 0))
+        self.methods = methods
+        self.user_group = nn.Linear(num_user_group, num_features)
+        self.book_author = nn.Linear(num_book_author, num_features)
+        self.book_publisher = nn.Linear(num_book_publisher, num_features)
+        self.book_year = nn.Linear(num_book_year, num_features)
+        self.user_group_weight_linear = nn.Linear(num_user_group, 1)
+        self.book_author_weight_linear = nn.Linear(num_book_author, 1)
+        self.book_publisher_weight_linear = nn.Linear(num_book_publisher, 1)
+        self.book_year_weight_linear = nn.Linear(num_book_year, 1)        
+        self.deep_decoder = nn.Sequential(
+            nn.Linear(num_deep_decoder, int(round(num_deep_decoder/2, 0))),
+            nn.Tanh(),
+            nn.Linear(int(round(num_deep_decoder/2, 0)), int(round(num_deep_decoder/4, 0))),
+            nn.Tanh(),
+        )
+        self.decoder = nn.Linear(num_decoder, 1)
+        return
+
+    def forward(self, user_group_feature, book_author_feature, book_publisher_feature, book_year_feature):
+        # Embedding Learning
+        self.user_group_embedding = self.user_group(user_group_feature) # shape=(batch_size, embedding)
+        self.book_author_embedding = self.book_author(book_author_feature) # shape=(batch_size, embedding)
+        self.book_publisher_embedding = self.book_publisher(book_publisher_feature) # shape=(batch_size, embedding)
+        self.book_year_embedding = self.book_year(book_year_feature) # shape=(batch_size, embedding)
+        self.user_group_weight = self.user_group_weight_linear(user_group_feature) # shape = (batch_size, 1)
+        self.book_author_weight = self.book_author_weight_linear(book_author_feature) # shape = (batch_size, 1)
+        self.book_publisher_weight = self.book_publisher_weight_linear(book_publisher_feature) # shape = (batch_size, 1)
+        self.book_year_weight = self.book_year_weight_linear(book_year_feature) # shape = (batch_size, 1)
+
+        ## Deep part
+        # Inner product
+        self.user_group_book_author = self.user_group * self.book_author
+        self.user_group_book_publisher = self.user_group * self.book_publisher
+        self.user_group_book_year = self.user_group * self.book_year
+        self.book_author_book_publisher = self.book_author * self.book_publisher
+        self.book_author_book_year = self.book_author * self.book_year
+        self.book_publisher_book_year = self.book_publisher * self.book_year
+
+        # Concatenate
+        self.deep_part_all = torch.cat((self.user_group_book_author, self.user_group_book_publisher,
+                                        self.user_group_book_year, self.book_author_book_publisher,
+                                        self.book_author_book_year, self.book_publisher_book_year), dim=-1) 
+
+        # Deep_Decoder
+        self.deep_part_all = self.deep_decoder(self.deep_part_all)
+
+        ## Wide part
+        # Concatenate
+        self.wide_part_all = torch.cat([self.user_group_embedding, self.book_author_embedding, self.book_publisher_embedding, self.book_year_embedding], dim=1)
+
+        ## All
+        # Concatenate
+        self.all = torch.cat([self.wide_part_all, self.deep_part_all], dim=1)
+
+        # Decoder
+        X = self.decoder(self.all)
+
         if self.methods == "regression":
             return X
         else:
